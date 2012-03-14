ea77373ef54e00847b0ffa30742674d096e3c36d
mllg <michellang@gmail.com>
2012-03-14 12:50:24 +0000

    removed FIXME, added comment, dont allow points in registry names

--- skel/R/checkIdValid.R
+++ skel/R/checkIdValid.R
@@ -1,10 +1,11 @@
 # check for a valid registry id:
 # must start with an alpha-numeric
-# then alpha-numerics, and underscores are allowed
-# FIXME This is stupid. I want a minus:
-# pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z._-]*$"
+# then alpha-numerics and underscores are allowed
+# pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z_-]*$"
+# we must be quite restrictive here because we prefix
+# the table name with the registry name
 checkIdValid = function(id) {
-  pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z._]*$"
+  pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z_]*$"
   if (!grepl(pattern, id))
     stopf("Id does not comply with pattern %s: %s", pattern, id)
 }
