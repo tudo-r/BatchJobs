d0aa717a6965a17ad12b3f20b8b159e4a9108046
berndbischl <bernd_bischl@gmx.net>
2012-03-21 23:43:53 +0000

    clean up tests

--- skel/inst/tests/test_batchReduceResults.R
+++ skel/inst/tests/test_batchReduceResults.R
@@ -5,17 +5,14 @@ test_that("batchReduceResults", {
   batchMap(reg, function(x) x^2, 1:5)
   submitJobs(reg)
 
-  td = tempdir()
-  unlink(td, recursive=TRUE)  
-  reg2 = makeRegistry(id="foo", file.dir = td)
-  batchReduceResults(reg, reg2, , block.size=3, init=c(),
+  reg2 = makeRegistry(id="foo", file.dir=tempfile())
+  batchReduceResults(reg, reg2, block.size=3, init=c(),
     fun=function(aggr, job, res) c(aggr, res))
   submitJobs(reg2)
   expect_equal(loadResult(reg2, getJobIds(reg)[1]), (1:3)^2, check.names=FALSE)
   expect_equal(loadResult(reg2, getJobIds(reg)[2]), (4:5)^2, check.names=FALSE)
   
-  unlink(td, recursive=TRUE)  
-  reg2 = makeRegistry(id="foo", file.dir = td)
+  reg2 = makeRegistry(id="foo", file.dir=tempfile())
   ids = getJobIds(reg)
   batchReduceResults(reg, reg2, ids=ids[2:4], block.size=2, init=c(), 
     fun=function(aggr, job, res) c(aggr, res))
@@ -23,3 +20,4 @@ test_that("batchReduceResults", {
   expect_equal(loadResult(reg2, getJobIds(reg)[1]), (2:3)^2, check.names=FALSE)
   expect_equal(loadResult(reg2, getJobIds(reg)[2]), (4)^2, check.names=FALSE)
 })
+
