0c56bebd1fb52fc00a8e0b942efda1e5b0bb4a45
berndbischl <bernd_bischl@gmx.net>
2012-03-22 22:28:38 +0000

    bj: more external tests

--- external_tests/bernd_office_winxp.R
+++ external_tests/bernd_office_winxp.R
@@ -6,9 +6,9 @@ conf$mail.start = conf$mail.done = conf$mail.error = "none"
 
 conf$cluster.functions = makeClusterFunctionsInteractive()
 doExternalTest(whitespace=FALSE)
-doExternalTest(whitespace=TRUE)
+#doExternalTest(whitespace=TRUE)
 
 conf$cluster.functions = makeClusterFunctionsLocal()
 doExternalTest(whitespace=FALSE)
-doExternalTest(whitespace=TRUE)
+#doExternalTest(whitespace=TRUE)
 
