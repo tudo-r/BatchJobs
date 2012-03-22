a5f8ae87abab73ee786e2241ed1bb91d83400176
berndbischl <bernd_bischl@gmx.net>
2012-03-22 19:54:34 +0000

    clean up

--- external_tests/helpers.R
+++ external_tests/helpers.R
@@ -11,7 +11,7 @@ doExternalTest = function(whitespace=FALSE) {
   xs = 1:5
   batchMap(reg, identity, xs)
   submitJobs(reg)
-  Sys.sleep(3)
+  Sys.sleep(5)
   res = reduceResults(reg, fun=function(aggr,job,res) c(aggr, res))
   expect_equal(res, xs)
 }
