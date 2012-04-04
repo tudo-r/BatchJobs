a668b782bdbf81e9c0010d83335491d5fbc0d36a
berndbischl <bernd_bischl@gmx.net>
2012-04-04 16:41:27 +0000

    bj: debug functions for multicore and ssh

---
+++ skel/R/debugMulticore.R
@@ -0,0 +1,72 @@
+#' Helper function to debug multicore mode.
+#'
+#' Useful in case of severe errors. 
+#' Tries different operations of increasing difficulty 
+#' and provides debug output on the console
+#' 
+#' @return Nothing.
+#' @export
+debugMulticore = function() {
+  conf = getBatchJobsConf()
+  conf$debug = TRUE
+  conf$mail.start = conf$mail.done = conf$mail.error = "none"
+  rhome = R.home()
+  
+  messagef("*** System info: ***")
+  print(Sys.info())
+  catf("\n") 
+  
+  messagef("*** which R: ***")
+  res = BatchJobs:::runCommand("which", "R")
+  messagef("which R result: %s", res)
+  catf("\n") 
+    
+  messagef("*** Find helper script: ***")
+  script = BatchJobs:::findHelperScriptLinux(rhome)
+  messagef("Find helper script result: %s", script)
+  catf("\n") 
+ 
+  messagef("*** Auto-detecting ncpus: ***")
+  worker = makeWorkerLocalLinux(script, ncpus=1)
+  ncpus = onWorkerLinux(worker, "number-of-cpus") 
+  messagef("Auto-detecting ncpus result: %s", ncpus)
+  catf("\n") 
+  
+  messagef("*** Query worker status: ***")
+  res = onWorkerLinux(worker, "status", args="") 
+  messagef("Query worker status result: %s", res)
+  catf("\n") 
+  
+  messagef("*** Submitting 1 job: ***")
+  conf$cluster.functions = makeClusterFunctionsMulticore()
+  fd = tempfile()
+  reg = makeRegistry(id = "debug_multicore", file.dir=fd, sharding=FALSE)
+  batchMap(reg, identity, 1)
+  submitJobs(reg)
+  Sys.sleep(3)
+  messagef("Submitting 1 job result: %i", loadResult(reg, 1))
+  messagef("Query worker status:")
+  res = onWorkerLinux(worker, "status", args=reg$file.dir) 
+  messagef("Query worker status result: %s", res)
+  catf("\n") 
+ 
+  messagef("*** Killing 2 jobs: ***")
+  fd = tempfile()
+  reg = makeRegistry(id = "debug_multicore", file.dir=fd, sharding=FALSE)
+  f = function(i) if(i <= 1) i else f(i-1) + f(i-2)
+  xs = 50 + seq(1,2)
+  ids = 1:2
+  batchMap(reg, f, xs)
+  submitJobs(reg)
+  Sys.sleep(3)
+  messagef("Query worker status:")
+  res = onWorkerLinux(worker, "status", args=reg$file.dir) 
+  messagef("Query worker status result: %s", res)
+  messagef("Running jobs: %s", collapse(findRunning(reg)))
+  killJobs(reg, ids)
+  messagef("Query worker status:")
+  res = onWorkerLinux(worker, "status", args=reg$file.dir) 
+  messagef("Query worker status result: %s", res)
+  messagef("Running jobs: %s", collapse(findRunning(reg)))
+  catf("\n") 
+}
---
+++ skel/R/debugSSH.R
@@ -0,0 +1,83 @@
+#' Helper function to debug SSH mode.
+#'
+#' Useful in case of severe errors. 
+#' Tries different operations of increasing difficulty 
+#' and provides debug output on the console
+#' 
+#' @return Nothing.
+#' @export
+debugSSH = function(nodenames, rhome=R.home()) {
+  n = length(nodenames)
+  if (length(rhome) == 1)
+    rhome = rep(rhome, n)
+  conf = getBatchJobsConf()
+  conf$debug = TRUE
+  conf$mail.start = conf$mail.done = conf$mail.error = "none"
+  
+  messagef("*** System info: ***")
+  print(Sys.info())
+  catf("\n") 
+  
+  messagef("*** which R: ***")
+  res = sapply(nodenames, BatchJobs:::runCommand, cmd="which", args="R", ssh=TRUE)
+  messagef("which R result:")
+  print(res) 
+  catf("\n") 
+  
+  messagef("*** Find helper script: ***")
+  res = Map(BatchJobs:::findHelperScriptLinux, rhome, nodenames, ssh=TRUE)
+  res = unlist(res); names(res) = nodenames
+  messagef("Find helper script result:")
+  print(res) 
+  catf("\n") 
+  
+  messagef("*** Auto-detecting ncpus: ***")
+  workers = Map(makeWorkerRemoteLinux, nodenames, rhome, ncpus=1)
+  res = lapply(workers, onWorkerLinux, command="number-of-cpus") 
+  messagef("Auto-detecting ncpus result:") 
+  print(res) 
+  catf("\n") 
+  
+  messagef("*** Query worker status: ***")
+  res = lapply(workers, onWorkerLinux, command="status", args="") 
+  messagef("Query worker status result:")
+  print(res) 
+  catf("\n") 
+  
+  messagef("*** Submitting 1 job: ***")
+  ssh.workers = Map(makeSSHWorker, nodenames, rhome)
+  conf$cluster.functions = do.call(makeClusterFunctionsSSH, ssh.workers)
+  fd = tempfile()
+  reg = makeRegistry(id = "debug_multicore", file.dir=fd, sharding=FALSE)
+  batchMap(reg, identity, 1)
+  submitJobs(reg)
+  Sys.sleep(3)
+  messagef("Submitting 1 job result: %i", loadResult(reg, 1))
+  messagef("*** Query worker status: ***")
+  res = lapply(workers, onWorkerLinux, command="status", args=reg$file.dir) 
+  messagef("Query worker status result:")
+  print(res) 
+  catf("\n") 
+  
+  messagef("*** Killing 2 jobs: ***")
+  fd = tempfile()
+  reg = makeRegistry(id = "debug_multicore", file.dir=fd, sharding=FALSE)
+  f = function(i) if(i <= 1) i else f(i-1) + f(i-2)
+  xs = 50 + seq(1,2)
+  ids = 1:2
+  batchMap(reg, f, xs)
+  submitJobs(reg)
+  Sys.sleep(3)
+  messagef("*** Query worker status: ***")
+  res = lapply(workers, onWorkerLinux, command="status", args=reg$file.dir) 
+  messagef("Query worker status result:")
+  print(res) 
+  messagef("Running jobs: %s", collapse(findRunning(reg)))
+  killJobs(reg, ids)
+  messagef("*** Query worker status: ***")
+  res = lapply(workers, onWorkerLinux, command="status", args=reg$file.dir) 
+  messagef("Query worker status result:")
+  print(res) 
+  messagef("Running jobs: %s", collapse(findRunning(reg)))
+  catf("\n") 
+}
