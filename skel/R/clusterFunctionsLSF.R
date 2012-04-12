93fed11a746d4a61265659527df5785bfd16644b
berndbischl <bernd_bischl@gmx.net>
2012-04-12 20:51:42 +0000

    bj: cfLSF

---
+++ skel/R/clusterFunctionsLSF.R
@@ -0,0 +1,74 @@
+#' Create cluster functions for LSF systems.
+#' 
+#' Job files are created based on the brew template
+#' \code{template.file}. This file is processed with brew and then
+#' submitted to the queue using the \code{bsub} command. Jobs are
+#' killed using the \code{bkill} command and the list of running jobs
+#' is retrieved using \code{bjobs -u $USER -w}. The user must have the
+#' appropriate privileges to submit, delete and list jobs on the
+#' cluster (this is usually the case).
+#'
+#' The template file can use any of the resources defined for the job
+#' by accessing the appropriate element of the \code{resources}
+#' list. It is the template file's job to choose a queue for the job
+#' and add any desired resource allocations. A simple example
+#' is provided here 
+#' \url{http://code.google.com/p/batchjobs/source/browse/trunk/BatchJobs/examples/cfLSF/simple.tmpl}
+#' in the package repository on its homepage.
+#'
+#' @param template.file [\code{character(1)}]\cr
+#'   Path to a brew template file that is used for the job file.
+#' @return [\code{\link{ClusterFunctions}}]. 
+#' @export
+makeClusterFunctionsLSF = function(template.file) {
+  checkArg(template.file, "character", len=1, na.ok=FALSE)
+  ## Read in template
+  fd = file(template.file, "r")
+  template = paste(readLines(fd), collapse="\n")
+  close(fd)
+  
+  submitJob = function(conf, reg, job.name, rscript, log.file, job.dir, resources) {
+    if (conf$debug) { 
+      # if not temp, use jobs dir
+      outfile = str_replace(rscript, "\\.R$", ".job")
+    } else {
+      outfile = tempfile()
+    }
+    brew(text=template, output=outfile)
+    # returns: "Job <128952> is submitted to default queue <s_amd>."
+    # FIXME: system3
+    res = list()
+    res$output = system2("bsub", stdin=outfile)  
+    res$exit.code = 0
+    # FIXME filled queues
+    if (res$exit.code > 0) {
+      msg = sprintf("bsub produced exit code %i; output %s", res$exit.code, res$output)
+      makeSubmitJobResult(status=101L, msg=msg)
+    } else {
+      # first number in string is batch.job.id
+      batch.job.id = str_extract(res$output, "\\d+")
+      makeSubmitJobResult(status=0L, batch.job.id=batch.job.id)
+    }
+  }
+  
+  killJob = function(conf, reg, batch.job.id) {
+    runCommand("bkill", batch.job.id)
+  }
+  
+  listJobs = function(conf, reg) {
+    # JOBID   USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME
+    # 106560  rogon   UNKWN m_amd      hpc84       hpc25       QScript    Mar 19 12:18
+    res = runCommand("bjobs", c("-u $USER", "-w"), stop.on.exit.code=FALSE)
+    if (res$exit.code == 255 && str_detect(res$output, "No unfinished job found"))
+      return(character(0))
+    if (res$exit.code > 0)
+      stopf("bjobs produced exit code %i; output %s", res$exit.code, res$output)
+    res = res$output  
+    # drop first header line
+    res = res[-1]
+    # first number in strings are batch.job.ids
+    str_extract(res, "\\d+")
+  }
+  
+  makeClusterFunctions(name="LSF", submitJob=submitJob, killJob=killJob, listJobs=listJobs)
+}
