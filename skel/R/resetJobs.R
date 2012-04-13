f9b6f5b2c77516e30c1222435d082724e774ebac
mllg <michellang@gmail.com>
2012-04-13 14:21:19 +0000

    bj: fixed typo in resetJobs

--- skel/R/resetJobs.R
+++ skel/R/resetJobs.R
@@ -2,10 +2,10 @@
 #'
 #' Reset state of jobs in the database. Useful under two circumstances:
 #' Either to re-submit them because of changes in e.g. external
-#' data or to resolve rare issues when jobs are killed in an unfortunate state 
+#' data or to resolve rare issues when jobs are killed in an unfortunate state
 #' and therefore blocking your registry.
 #' Note that this is a dangerous operation to perform which may harm
-#' the database integrity. You HAVE to externally make sure that none of the jobs 
+#' the database integrity. You HAVE to externally make sure that none of the jobs
 #' you want to reset are still running.
 #' If you really know what you are
 #' doing, you may set \code{force} to \code{TRUE} to omit sanity checks
@@ -32,16 +32,16 @@ resetJobs = function(reg, ids, force=FALSE) {
   }
   checkArg(force, cl="logical", len=1L, na.ok=FALSE)
   if (!force) {
-    if(is.null(getListJob()) || is.null(getKillJob())) {
+    if(is.null(getListJobs()) || is.null(getKillJob())) {
       stop("Listing or killing of jobs not supported by your cluster functions\n",
            "You need to set force=TRUE to reset jobs, but note the warning in ?resetJobs")
     }
     running = findRunning(reg, ids)
     if (length(running) > 0L)
-      stopf("Can't reset jobs which are still running. You have to kill them first.\nRunning: %s", 
+      stopf("Can't reset jobs which are still running. You have to kill them first.\nRunning: %s",
             collapse(running))
   }
-  
+
   messagef("For safety reasons waiting 3secs...")
   Sys.sleep(3)
   messagef("Resetting %i jobs in DB.", length(ids))
