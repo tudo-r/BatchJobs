ba1f04231248470ac8951777cd543d1f359e16c3
mllg <michellang@gmail.com>
2012-04-21 10:49:00 +0000

    test fixed

--- skel/inst/tests/test_getJob.R
+++ skel/inst/tests/test_getJob.R
@@ -1,6 +1,6 @@
 context("getJob")
 
-test_that("getJob", {  
+test_that("getJob", {
   reg = makeTestRegistry()
   batchMap(reg, identity, 1:3)
   ids = getJobIds(reg)
@@ -8,7 +8,7 @@ test_that("getJob", {
   expect_equal(j$id, ids[2])
   expect_equal(j$pars, list(2))
   expect_is(j, "Job")
-  
+
   js = getJobs(reg, ids[2:3])
   expect_is(js, "list")
   expect_equal(length(js), 2)
@@ -18,15 +18,15 @@ test_that("getJob", {
   expect_equal(js[[2]]$id, ids[3])
   expect_equal(js[[1]]$pars, list(2))
   expect_equal(js[[2]]$pars, list(3))
-  
+
   # empty ids
   expect_equal(getJobs(reg, integer(0)), list())
   # bad id
-  expect_error(getJob(reg, "xxx"), "Ids not present in registry: xxx")
-  expect_error(getJobs(reg, "xxx"), "Ids not present in registry: xxx") 
+  expect_error(getJob(reg, "xxx"), "integer")
+  expect_error(getJobs(reg, -1), "Ids not present in registry: -1")
 })
 
-test_that("getJob returns jobs in same order as ids", {  
+test_that("getJob returns jobs in same order as ids", {
   reg = makeTestRegistry()
   batchMap(reg, identity, 1:3)
   ids = getJobIds(reg)
