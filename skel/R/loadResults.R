472b7732d4caf3ce602f442e539aa13a9817acc9
berndbischl <bernd_bischl@gmx.net>
2012-04-22 16:14:38 +0000

    bj: loadResults

---
+++ skel/R/loadResults.R
@@ -0,0 +1,43 @@
+#' Loads result files for id vector.
+#' @param reg [\code{\link{Registry}}]\cr
+#'   Registry.
+#' @param ids [\code{integer}]\cr
+#'   Ids of jobs.
+#'   Default is all.
+#' @param part [\code{character(1)}]
+#'   Only useful for multiple result files, then defines which result file part should be loaded.
+#'   \code{NA} means all parts are loaded, which is the default.
+#' @param check.ids [\code{logical(1)}]\cr
+#'   Check the job ids?
+#'   Default is \code{TRUE}.
+#' @param e.names [\code{logical(1)}]\cr
+#'   Should the result be simplified to a vector, matrix or higher dimensional array if possible?
+#'   Default is \code{TRUE}.
+#' @param use.names [\code{logical(1)}]\cr
+#'   Should the returned list be named with the ids?
+#'   Default is \code{TRUE}.
+#' @return [\code{list}]. Results of jobs as list, possibly named by ids.
+#' @seealso \code{\link{reduceResults}}
+#' @export
+loadResults = function(reg, ids, part=as.character(NA), simplify=FALSE, use.names=TRUE, check.ids=TRUE) {
+  checkArg(reg, "Registry")
+  if (missing(ids)) {
+    ids = getJobIds(reg)
+  } else {
+    if(check.ids) {
+      ids = convertIntegers(ids)
+      checkArg(ids, "integer", na.ok=FALSE)
+      checkIds(reg, id)
+    }
+  }
+  checkArg(simplify, "logical", len=1, na.ok=FALSE)
+  checkArg(use.names, "logical", len=1, na.ok=FALSE)
+  checkArg(check.ids, "logical", len=1, na.ok=FALSE)
+  if (length(ids) == 0)
+    return(list())
+  res = sapply(ids, loadResult, reg=reg, part=part, check.id=FALSE, 
+    simplify=simplify, USE.NAMES=FALSE)
+  if (use.names)
+    names(res) = ids
+  return(res)
+}
---
+++ skel/inst/tests/test_loadResults.R
@@ -0,0 +1,30 @@
+context("submitJobs")
+
+  
+test_that("loadResults", {
+  reg = makeTestRegistry()
+  ids = 1:2
+  batchMap(reg, identity, ids)
+  submitJobs(reg)
+  ys1 = 1:2
+  ys2 = loadResults(reg, simplify=TRUE, use.names=FALSE)
+  expect_equal(ys1, ys2)
+  ys1 = as.list(ys1)
+  ys2 = loadResults(reg, simplify=FALSE, use.names=FALSE)
+  expect_equal(ys1, ys2)
+  names(ys1) = ids
+  ys2 = loadResults(reg, simplify=FALSE, use.names=TRUE)
+  expect_equal(ys1, ys2)
+  ys1 = unlist(ys1)
+  ys2 = loadResults(reg, simplify=TRUE, use.names=TRUE)
+  expect_equal(ys1, ys2)
+
+  ys2 = loadResults(reg, 2)
+  expect_equal(list("2"=2), ys2)
+  
+  expect_equal(loadResults(reg, ids=integer(0), simplify=TRUE, use.names=TRUE), list())
+  expect_equal(loadResults(reg, ids=integer(0), simplify=FALSE, use.names=TRUE), list())
+  expect_equal(loadResults(reg, ids=integer(0), simplify=TRUE, use.names=FALSE), list())
+  expect_equal(loadResults(reg, ids=integer(0), simplify=FALSE, use.names=FALSE), list())
+})
+
