b0adb07dff76c52bad140d7ef82fb4ccf510336f
berndbischl <bernd_bischl@gmx.net>
2012-04-23 16:52:47 +0000

    bj: new reduceResults conenience functions

--- skel/R/reduceResults.R
+++ skel/R/reduceResults.R
@@ -81,3 +81,77 @@ reduceResults = function(reg, ids, part=as.character(NA), fun, init, ...) {
 
   return(aggr)
 }
+
+reduceResultsReturnVal = function(reg, ids, part, fun, wrap, combine, use.names, name.fun, ..., init) {
+  checkArg(reg, "Registry")
+  if (missing(ids))
+    ids = findDone(reg)
+  if (missing(fun)){
+    fun = function(job, res) res
+  } else {
+    force(fun)
+    checkArg(fun, formals=c("job", "res"))
+  }
+  fun2 = function(aggr, job, res) combine(aggr, wrap(fun(job, res)))
+  res = reduceResults(reg, ids, part, fun2, init, ...)
+  if (use.names)
+    res = name.fun(res, ids, fun(getJob(reg, ids[1]), loadResult(reg, ids[1])))
+  return(res)  
+}
+
+
+#' Reduce results into specific data structure.
+#' @param reg [\code{\link{Registry}}]\cr
+#'   Registry.
+#' @param ids [\code{integer}]\cr
+#'   Ids of selected jobs.
+#'   Default is all jobs for which results are available.
+#' @param part [\code{character(1)}]\cr
+#'   Only useful for multiple result files, then defines which result file part should be loaded.
+#'   \code{NA} means all parts are loaded, which is the default.
+#' @param fun [\code{function(job, res)}]\cr
+#'   Function used to select relevant parts of results. 
+#'   \code{job} is the current job descriptor and \code{result} is the current result object.
+#'   Default is to return \code{res}.
+#' @param ... [any]\cr
+#'   Additional arguments to \code{fun}.
+#' @param use.names [\code{logical(1)}]\cr
+#'   Should the return value be named?
+#'   Default is \code{TRUE}.
+#' @return Aggregated results, return type depends on function.
+#' @export
+#' @rdname reduceResultsVector
+reduceResultsVector = function(reg, ids, part=as.character(NA), fun, ..., use.names=TRUE) {
+  nf = function(res, ids, x1) {names(res) = ids; res} 
+  reduceResultsReturnVal(reg, ids, part, fun, identity, c, use.names, nf, ..., init=c())
+}
+
+#' @export
+#' @rdname reduceResultsVector
+reduceResultsList = function(reg, ids, part=as.character(NA), fun, ..., use.names=TRUE) {
+  nf = function(res, ids, x1) {names(res) = ids; res} 
+  reduceResultsReturnVal(reg, ids, part, fun, list, c, use.names, nf, ..., init=list())
+}
+
+#' @export
+#' @rdname reduceResultsVector
+reduceResultsMatrix = function(reg, ids, part=as.character(NA), fun, ..., rows=TRUE, use.names=TRUE) {
+  combine = if (rows) rbind else cbind
+  if (rows)
+    nf = function(res, ids, x1) {rownames(res) = ids; colnames(res) = names(x1); res}
+  else 
+    nf = function(res, ids, x1) {colnames(res) = ids; rownames(res) = names(x1); res}
+  res = reduceResultsReturnVal(reg, ids, part, fun, unlist, combine, use.names, nf, ..., init=c())
+  if (!use.names)
+    dimnames(res) = NULL
+  return(res)
+}
+
+#' @export
+#' @rdname reduceResultsVector
+reduceResultsDataFrame = function(reg, ids, part=as.character(NA), fun, ...) {
+  nf = function(res, ids, x1) {rownames(res) = ids; colnames(res) = names(x1); res}
+  reduceResultsReturnVal(reg, ids, part, fun, as.data.frame, rbind, TRUE, nf, ..., init=data.frame())
+}
+
+  
