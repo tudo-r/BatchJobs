3805c7a5a8071a352efca816c8120b1f6c18cc44
mllg <michellang@gmail.com>
2012-05-10 09:37:11 +0000

    parameter ids added for findJobs

--- skel/R/findJobs.R
+++ skel/R/findJobs.R
@@ -2,10 +2,13 @@
 #'
 #' @param reg [\code{\link{Registry}}]\cr
 #'   Registry.
+#' @param ids [\code{integer}]\cr
+#'   Subset of job ids to restrict the result to.
+#'   Default is all jobs.
 #' @param pars [quoted R expression]\cr
 #'   All jobs whose parameters match the given expression are selected.
 #'   This implies that you have named the parameters when you passed the vectors.
-#'   If you forgot to do this you can use \code{.arg1}, \code{.arg2}, etc., to refer to the 
+#'   If you forgot to do this you can use \code{.arg1}, \code{.arg2}, etc., to refer to the
 #'   the unnamed ones.
 #' @return [\code{integer}]. Ids for jobs which match the query.
 #' @export
@@ -14,12 +17,17 @@
 #' f <- function(x, y) x * y
 #' batchExpandGrid(reg, f, x=1:2, y=1:3)
 #' findJobs(reg, pars=quote(y > 2))
-findJobs = function(reg, pars) {
+findJobs = function(reg, ids, pars) {
   checkArg(reg, cl="Registry")
-  jobs = getJobs(reg, check.ids=FALSE)
+  if (!missing(ids))
+    ids = checkIds(reg, id)
+  if (!is.evaluable(pars))
+    stop("Argument 'pars' must be a call, expression or symbol!")
+  jobs = getJobs(reg, ids, check.ids=FALSE)
+
   rename = function(pars) {
     ns = names(pars)
-    if (is.null(ns)) {  
+    if (is.null(ns)) {
       ns = rep("", length(pars))
     }
     j = which(is.na(ns) | ns == "")
@@ -27,9 +35,7 @@ findJobs = function(reg, pars) {
     names(pars) = ns
     pars
   }
-  if (!is.evaluable(pars))
-    stop("Argument 'pars' must be a call, expression or symbol!")
+
   jobs = Filter(function(j) eval(pars, rename(j$pars)), jobs)
   return(extractSubList(jobs, "id", element.value=integer(1L)))
 }
-
