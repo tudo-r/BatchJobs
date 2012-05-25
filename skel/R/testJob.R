692589ddc7b13a6f01fbdf7d57567d98eaa20e66
berndbischl <bernd_bischl@gmx.net>
2012-05-25 16:47:47 +0000

    clean up

--- skel/R/testJob.R
+++ skel/R/testJob.R
@@ -11,11 +11,13 @@
 #' @seealso \code{\link{reduceResults}}
 #' @export
 #' @examples
+#' \dontrun{
 #' reg <- makeRegistry(id="BatchJobsExample", file.dir=tempfile(), seed=123)
 #' f <- function(x) if (x==1) stop("oops") else x
 #' batchMap(reg, f, 1:2)
 #' testJob(reg, 1)
 #' testJob(reg, 2)
+#'}
 testJob = function(reg, id) {
   checkArg(reg, cl="Registry")
   if (missing(id)) {
