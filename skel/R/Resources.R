e84df07669ea296b5175898ff7aa2b7d22a3a14a
berndbischl <bernd_bischl@gmx.net>
2012-06-29 20:04:08 +0000

    bj: resources stored in file dir and are avail on slave

---
+++ skel/R/Resources.R
@@ -0,0 +1,91 @@
+# FIXME:
+# which standard names
+
+resrc = function(nodes, walltime, memory, ...) {
+  resources = getBatchJobsConf()$default.resources
+  if (!missing(nodes)) {
+    resources$nodes = nodes
+  }
+  if (!missing(walltime)) {
+    resources$walltime = walltime
+  }
+  if (!missing(memory)) {
+    resources$memory = memory
+  }
+  if (!is.null(resources$nodes)) {
+    resources$nodes = convertInteger(resources$nodes)
+    checkArg(resources$nodes, "integer", len=1L, lower=1L, na.ok=FALSE)
+  }
+  if (!is.null(resources$walltime)) {
+    resources$walltime = convertInteger(resources$walltime)
+    checkArg(resources$walltime, "integer", len=1L, lower=1L, na.ok=FALSE)
+  }
+  if (!is.null(resources$memory)) {
+    resources$memory = convertInteger(resources$memory)
+    checkArg(resources$memory, "integer", len=1L, lower=1L, na.ok=FALSE)
+  }
+  res2 = list(...)
+  if(!isProperlyNamed(res2))
+    stop("Resources must be all be uniquely named!")
+  resources = insert(resources, res2)
+  return(resources)
+}
+
+saveResources = function(reg, resources, timestamp = as.integer(Sys.time())) {
+  fn = getResourcesFilePath(reg, timestamp)
+  save2(file=fn, resources=resources)
+  return(timestamp)
+}
+
+#' Function to get job resources in job function.
+#' 
+#' Return the list passed to \code{\link{submitJobs}}, e.g. 
+#' nodes, walltime, etc. 
+#' 
+#' Can only be called in job function during job execution on slave.
+#' 
+#' @return [\code{list}].
+#' @export
+getResources = function() { 
+  if (!isOnSlave())
+    stop("getResources can only be called during job execution on slave!")
+  load2(getOption("BatchJobs.resources.path"))  
+}
+
+#' Function to get the resources that were submitted for some jobs.
+#' 
+#' Throws an error if call it for unsubmitted jobs.
+#' 
+#' @param reg [\code{\link{Registry}}]\cr
+#'   Registry.
+#' @param ids [\code{integer}]\cr
+#'   Ids of jobs.
+#'   Default is all submitted jobs.
+#' @param as.list [\code{integer(1)}]\cr
+#'   If \code{FALSE}, \code{rbind.fill} is called on the result to
+#'   convert it to a data.frame.
+#'   Default is \code{TRUE}.
+#' @return [\code{list} | \code{data.frame}]. List (or data.frame) of resource lists as passed to \code{\link{submitJobs}}.
+#' @export
+getJobResources = function(reg, ids, as.list=TRUE) {  
+  checkArg(reg, "Registry")
+  if (missing(ids)) {
+    ids = findSubmitted(reg)
+  } else {
+    ids = checkIds(reg, ids)
+    nsub = ids[ids %nin% findSubmitted(reg)]
+    if (length(nsub > 0))
+      stopf("Some of your jobs have not been submitted, so no resources are available, e.g. for id=%i", nsub[1])
+  } 
+  query = sprintf("SELECT job_id, resources_timestamp FROM %s_job_status", reg$id)
+  df = dbSelectWithIds(reg, query, ids)
+  res = namedList(df$job_id)
+  for(ts in unique(df$resources_timestamp)) {
+    res[df$resources_timestamp == ts] = load2(getResourcesFilePath(reg, ts), simplify=FALSE)
+  }         
+  if (!as.list)
+    res = do.call(rbind.fill, lapply(res, as.data.frame))
+  return(res)              
+}
+
+# FIXME: what do we do with the DB if resources were missing (oldversion) and return in tis case?
---
+++ skel/inst/tests/test_resources.R
@@ -0,0 +1,43 @@
+context("resources")
+
+test_that("resources", {  
+  # use resources on slave
+  reg = makeTestRegistry()
+  batchMap(reg, function(i) getResources(), 1:2)
+  submitJobs(reg, resources=list(walltime=1, memory=2))
+  res = list(walltime=1, memory=2)
+  expect_equal(loadResult(reg, 1), res)
+  expect_equal(testJob(reg, 1, resources=res), res)
+  
+  # query on master
+  res1 = getJobResources(reg)
+  res2 = getJobResources(reg, 1:2)
+  expect_equal(res1, res2)
+  expect_equal(res1, list('1'=res, '2'=res))
+  
+  #submit only a few jobs
+  resetJobs(reg, ids=1:2, force=TRUE)
+  submitJobs(reg, ids=1, resources=list(walltime=1, memory=2))
+  expect_equal(loadResult(reg, 1), res)
+  expect_error(getJobResources(reg, 1:2), "not been submitted")
+  res1 = getJobResources(reg)
+  res2 = getJobResources(reg, 1)
+  expect_equal(res1, res2)
+  expect_equal(res1, list('1'=res))
+  
+  # defaults in conf
+  if (interactive()) 
+    conf = getBatchJobsConf()
+  else
+    conf = BatchJobs:::getBatchJobsConf()
+  conf$default.resources = list(walltime=1, memory=2, xxx=3)
+  reg = makeTestRegistry()
+  batchMap(reg, function(i) getResources(), 1)
+  submitJobs(reg, resources=list(memory=20))
+  res = list(walltime=1, memory=20, xxx=3)
+  expect_equal(loadResult(reg, 1), res)
+  expect_equal(getJobResources(reg)[[1]], res)
+  conf$default.resources = list()
+})
+
+
