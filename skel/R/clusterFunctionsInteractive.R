613d6defa0c1eb5e7ce012d3f64605680549438e
mllg <michellang@gmail.com>
2012-06-29 14:39:00 +0000

    cfInteractive now supports log files

--- skel/R/clusterFunctionsInteractive.R
+++ skel/R/clusterFunctionsInteractive.R
@@ -9,14 +9,39 @@
 #' Both killing and listing jobs is not supported by this
 #' implementation since at any one time there can be only one running
 #' job and while it is running, the master R process is blocked.
+# FIXME documentation
 #'
 #' @return [\code{\link{ClusterFunctions}}].
 #' @export
 makeClusterFunctionsInteractive = function() {
   submitJob = function(conf, reg, job.name, rscript, log.file, job.dir, resources) {
-    suppressAll(sys.source(rscript, envir=new.env(), keep.source=FALSE))
+    # open log file for writing
+    fn = file(log.file, open="wt")
+    on.exit(close(fn))
+
+    # set warn option to not collect warnings but signal them immedeiatly
+    # so they get included in the log file
+    prev.warn = getOption("warn")
+    on.exit(options(warn = prev.warn, add=TRUE))
+    if ("warning.as.error" %in% resources && isTRUE(resources$warning.as.error)) {
+      # convert warnings to errros
+      options(warn = 2L)
+    } else {
+      # immedeiatly output warnings
+      options(warn = 1L)
+    }
+
+    # sink both output and message streams
+    sink(fn, type="output")
+    sink(fn, type="message")
+    try(sys.source(rscript, envir=new.env(), keep.source=FALSE))
+    sink(NULL, type="output")
+    sink(NULL, type="message")
+
+    # return job result (always successful)
     makeSubmitJobResult(status=0L, batch.job.id="cfInteractive", msg="")
   }
+
   killJob = function(conf, reg, batch.job.id) NULL
   listJobs = function(conf, reg) integer(0L)
 
