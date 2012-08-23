e758facdbc45713b2c96e6d6f02bf62696099986
mllg <michellang@gmail.com>
2012-08-23 15:54:30 +0000

    dropped option use.names in getJobTimes

--- skel/R/getJobTimes.R
+++ skel/R/getJobTimes.R
@@ -4,22 +4,15 @@
 #' @param ids [\code{integer}]\cr
 #'   Ids of jobs.
 #'   Default is all jobs.
-#' @param use.names [\code{logical(1)}]\cr
-#'   Should result vector be named with \code{ids}?
-#'   Default is \code{TRUE}.
 #' @return [\code{integer}]. Time in seconds for jobs, named with ids.
 #'   \code{NA} if job has not terminated successfully.
 #' @export
-getJobTimes = function(reg, ids, use.names=TRUE) {
+getJobTimes = function(reg, ids) {
   checkArg(reg, cl = "Registry")
   if (!missing(ids))
     ids = checkIds(reg, ids)
-  checkArg(use.names, "logical", len=1L, na.ok=FALSE)
 
   tab = dbGetJobTimes(reg, ids)
-  if (!use.names)
-    return(tab$time)
-
   times = tab$time
   names(times) = tab$job_id
   return(times)
