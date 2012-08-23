1f6dd26ae73e2587d63eb099c0ab97b58044fbe9
mllg <michellang@gmail.com>
2012-08-23 15:35:23 +0000

    getErrors helper to retrieve error messages from database w/o using showStatus

---
+++ skel/R/getErrors.R
@@ -0,0 +1,34 @@
+#' Get error messages of jobs.
+#' @param reg [\code{\link{Registry}}]\cr
+#'   Registry.
+#' @param ids [\code{integer}]\cr
+#'   Ids of jobs.
+#'   Default is all jobs.
+#' @param print [\code{logical(1)}]\cr
+#'   Print error messages?
+#'   Default is \code{TRUE}.
+#' @return [\code{character}]. Error messages for jobs as character vector invisibly returend.\cr
+#'   \code{NA} if job has terminated successfully.
+#' @export
+getErrors = function(reg, ids, print=TRUE) {
+  checkArg(reg, cl = "Registry")
+  if (!missing(ids))
+    ids = checkIds(reg, ids)
+  checkArg(print, "logical", len=1L, na.ok=FALSE)
+
+  tab = dbGetErrorMsgs(reg, ids, filter=FALSE)
+  msgs = tab$error
+  names(msgs) = tab$job_id
+  tab = tab[!is.na(tab$error),, drop=FALSE]
+
+  if (print) {
+    if (nrow(tab) == 0L) {
+      catf("No errors")
+    } else {
+      catf("Showing %i errors:", nrow(tab))
+      cat(sprintf("Error in %i: %s", tab$job_id, tab$error), sep = "\n")
+    }
+  }
+
+  invisible(msgs)
+}
