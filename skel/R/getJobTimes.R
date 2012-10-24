3b2a66b0cef3eac4ca25e3a01b3581a32432798d
mllg <michellang@gmail.com>
2012-10-24 13:10:22 +0000

    getJobTimes: extra option "unit"

--- skel/R/getJobTimes.R
+++ skel/R/getJobTimes.R
@@ -4,14 +4,22 @@
 #' @param ids [\code{integer}]\cr
 #'   Ids of jobs.
 #'   Default is all jobs.
-#' @return [\code{integer}]. Time in seconds for jobs, named with ids.
+#' @param unit [\code{character}]\cr
+#'   Unit to convert execution times to.
+#'   Possible values: \dQuote{seconds}, \dQuote{minutes}, \dQuote{hours},
+#'   \dQuote{days} and \dQuote{weeks}.
+#'   Default is \dQuote{seconds}.
+#' @return [\code{numeric}]. Time in \code{unit} for jobs, named with ids.
 #'   \code{NA} if job has not terminated successfully.
 #' @export
-getJobTimes = function(reg, ids) {
+getJobTimes = function(reg, ids, unit = "seconds") {
   checkArg(reg, cl = "Registry")
-  if (!missing(ids))
+  if (missing(ids))
+    ids = dbGetJobIds(reg)
+  else
     ids = checkIds(reg, ids)
-
-  tab = dbGetJobTimes(reg, ids)
-  setNames(tab$time, tab$job_id)
+  conv = setNames(c(1L, 60L, 3600L, 86400L, 604800L),
+                  c("seconds", "minutes", "hours", "days", "weeks"))
+  checkArg(unit, choices = names(conv))
+  setNames(dbGetJobTimes(reg, ids)$time / conv[unit], ids)
 }
