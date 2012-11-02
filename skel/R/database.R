5747a15d8f6349c97a8813e1a33a82bbd2c19e56
mllg <michellang@gmail.com>
2012-11-02 15:05:06 +0000

    added vanished functions again ...

--- skel/R/database.R
+++ skel/R/database.R
@@ -235,6 +235,26 @@ dbGetMissingResults = function(reg, ids) {
   dbSelectWithIds(reg, query, ids, where=FALSE)$job_id
 }
 
+dbGetErrors = function(reg, ids) {
+  query = sprintf("SELECT job_id FROM %s_job_status WHERE error IS NOT NULL", reg$id)
+  dbSelectWithIds(reg, query, where=FALSE)$job_id
+}
+
+dbGetSubmitted = function(reg, ids) {
+  query = sprintf("SELECT job_id FROM %s_job_status WHERE submitted IS NOT NULL", reg$id)
+  dbSelectWithIds(reg, query, ids, where=FALSE)$job_id
+}
+
+dbGetNotSubmitted = function(reg, ids) {
+  query = sprintf("SELECT job_id FROM %s_job_status WHERE submitted IS NULL", reg$id)
+  dbSelectWithIds(reg, query, ids, where=FALSE)$job_id
+}
+
+dbGetStarted = function(reg, ids) {
+  query = sprintf("SELECT job_id FROM %s_job_status WHERE started IS NOT NULL", reg$id)
+  dbSelectWithIds(reg, query, ids, where=FALSE)$job_id
+}
+
 dbFindOnSystem = function(reg, ids) {
   fun = getListJobs("Cannot find jobs on system")
   batch.job.ids = fun(getBatchJobsConf(), reg)
