d43257f264fa5e081d3913fd8f3b215564efdcc1
mllg <michellang@gmail.com>
2012-11-08 09:55:23 +0000

    added unittest

---
+++ skel/inst/tests/test_sqlquotes.R
@@ -0,0 +1,21 @@
+context("sqlquotes")
+
+test_that("sqlquotes", {
+  reg = makeTestRegistry()
+  f = function(x) stop("Quoting is 'on'")
+  batchMap(reg, f, 1:2)
+  submitJobs(reg)
+  expect_equal(findErrors(reg), 1:2)
+
+  reg = makeTestRegistry()
+  f = function(x) stop('Quoting is "on"')
+  batchMap(reg, f, 1:2)
+  submitJobs(reg)
+  expect_equal(findErrors(reg), 1:2)
+
+  reg = makeTestRegistry()
+  f = function(x) stop("Some special chars: \n{}\r\t(),;")
+  batchMap(reg, f, 1:2)
+  submitJobs(reg)
+  expect_equal(findErrors(reg), 1:2)
+})
