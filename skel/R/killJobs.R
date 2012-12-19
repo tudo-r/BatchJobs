2cb614a3117579639b8db5a5505a9e36276a727b
mllg <michellang@gmail.com>
2012-12-19 10:30:44 +0000

    cleanup of killJobs fixed the bug that jobs could not get resetted in db

--- skel/R/killJobs.R
+++ skel/R/killJobs.R
@@ -29,83 +29,72 @@ killJobs = function(reg, ids) {
   else
     ids = checkIds(reg, ids)
 
-  printInfo = function() {
-    messagef("Trying to kill %i jobs.", length(ids))
-    ids.onsys = findOnSystem(reg, ids)
-    data = dbGetJobStatusTable(reg, ids.onsys)
-    n.unsubm = sum(is.na(data$submitted))
-    n.nobji = sum(is.na(data$batch_job_id))
-    n.term = sum(!is.na(data$done) | !is.na(data$error))
-    messagef("Jobs on system: %i", length(ids.onsys))
-    messagef("Of these: %i not submitted, %i with no batch.job.id, %i already terminated",
-     n.unsubm, n.nobji, n.term)
-    messagef("Killing real batch jobs: %i", length(ids.batch))
-  }
-
-  printBjis = function() {
-    # show first batch.job.ids
-    cutoff = cumsum(nchar(ids.batch) + 1L) > 200L
-    if (any(cutoff))
-      ids.str = paste(collapse(head(ids.batch, head(which(cutoff), 1L))), ",...", sep="")
-    else
-      ids.str = collapse(ids.batch)
-    message(ids.str)
-  }
-
-  killThem = function(bjis) {
-    old.warn = getOption("warn")
-    options(warn=0L)
-    bar = makeProgressBar(min=0L, max=length(bjis), label="killJobs")
-    bar$set()
-    notkilled = character(0L)
-    for (i in seq_along(bjis)) {
-      bji = bjis[i]
-      ok = try(killfun(conf, reg, bji))
-      if (is.error(ok)) {
-        notkilled = c(notkilled, bji)
-        warning(as.character(ok))
-      }
-      bar$inc(1L)
-    }
-    bar$kill()
-    options(warn=old.warn)
-    return(notkilled)
-  }
-
   conf = getBatchJobsConf()
   killfun = getKillJob("Cannot kill jobs")
-  ids.onsys = findOnSystem(reg, ids)
-  data = dbGetJobStatusTable(reg, ids.onsys)
-  # must be submitted, be not done, no error, has bji
-  data.subset = subset(data, !is.na(data$submitted) & is.na(data$done)
-    & is.na(data$error) & !is.na(data$batch_job_id))
-  # unique because of chunking
-  ids.batch = unique(data.subset$batch_job_id)
-  ids.job = data.subset$job_id
+  data = dbGetJobStatusTable(reg, ids = findOnSystem(reg, ids), # FIXME registry is already checked and sync'd ...
+                             cols = c("job_id", "batch_job_id", "submitted", "done", "error"))
 
-  printInfo()
-  if (length(ids.batch) == 0L) {
+  # print first summary information on jobs to kill
+  messagef("Trying to kill %i jobs.", length(ids))
+  messagef("Jobs on system: %i", nrow(data))
+  messagef("Of these: %i not submitted, %i with no batch.job.id, %i already terminated",
+           sum(is.na(data$submitted)), sum(is.na(data$batch_job_id)), sum(!is.na(data$done) | !is.na(data$error)))
+
+  # subset data: restrict to jobs submitted, not done, no error, has bji
+  # other jobs can be ignored -> overwrite ids
+  data = subset(data, !is.na(data$submitted) & is.na(data$done) & is.na(data$error) & !is.na(data$batch_job_id),
+                select = c("job_id", "batch_job_id"))
+  ids = data$job_id
+  bjids = unique(data$batch_job_id)
+  messagef("Killing real batch jobs: %i", length(bjids))
+
+  if (length(bjids) == 0L) {
     message("No batch jobs to kill.")
     return(invisible(integer(0L)))
   }
 
-  if (length(ids.batch) > 0L) {
-    printBjis()
-    notkilled = killThem(ids.batch)
-    if (length(notkilled) > 0L) {
-      messagef("Could not kill %i batch jobs, trying again.", length(notkilled))
-      Sys.sleep(2)
-      notkilled = killThem(notkilled)
+  doKill = function(bjids) {
+    n = length(bjids)
+    old.warn = getOption("warn")
+    bar = makeProgressBar(min=0L, max=n, label="killJobs")
+    on.exit({ options(warn = old.warn); bar$kill() })
+
+    options(warn = 0L)
+    bar$set()
+    notkilled = logical(n)
+
+    for (i in seq_len(n)) {
+      ok = try(killfun(conf, reg, bjids[i]))
+      if (is.error(ok)) {
+        notkilled[i] = TRUE
+        warning(as.character(ok))
+      }
+      bar$set(i)
     }
-  }
-  if (length(notkilled) > 0L) {
-    messagef("Could not kill %i batch jobs, kill them manually!\nTheir ids have been saved in .GlobalEnv under .batch.job.ids", length(notkilled))
-    assign(".batch.job.ids", notkilled, envir=.GlobalEnv)
-    # only reset killed jobs
-    ids.job = subset(data.subset, data.subset$batch_job_id %nin% notkilled)
+
+    bjids[notkilled]
   }
 
-  messagef("Resetting %i jobs in DB.", length(ids.job))
-  dbSendMessage(reg, dbMakeMessageKilled(reg, ids.job), staged = useStagedQueries())
-  invisible(ids.job)
+  # first try to kill
+  message(shortenString(collapse(bjids), 200L, ",..."))
+  bjids.notkilled = doKill(bjids)
+
+  # second try to kill
+  if (length(bjids.notkilled) > 0L) {
+    messagef("Could not kill %i batch jobs, trying again.", length(bjids.notkilled))
+    Sys.sleep(2)
+    bjids.notkilled = doKill(bjids.notkilled)
+  }
+
+  # second try also not successful
+  if (length(bjids.notkilled) > 0L) {
+    messagef("Could not kill %i batch jobs, kill them manually!\nTheir ids have been saved in .GlobalEnv under .batch.job.ids", length(bjids.notkilled))
+    assign(".batch.job.ids", bjids.notkilled, envir=.GlobalEnv)
+  }
+
+  # reset killed jobs
+  ids = ids[data$batch_job_id %nin% bjids.notkilled]
+  messagef("Resetting %i jobs in DB.", length(ids))
+  dbSendMessage(reg, dbMakeMessageKilled(reg, ids), staged = FALSE)
+  invisible(ids)
 }
