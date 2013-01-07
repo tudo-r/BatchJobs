40f14bf45b1343b1b4bb0642e2288638fb425923
mllg <michellang@gmail.com>
2013-01-07 13:48:37 +0000

    fix for showLog (RStudio sets pager option to a function in windows) sidenote: Rstudio on Win32 crashes on file.show. todo

--- skel/R/showLog.R
+++ skel/R/showLog.R
@@ -4,18 +4,24 @@
 #'
 #' Note this rare special case: When you use chunking, submit some jobs, some jobs fail,
 #' then you resubmit these jobs again in different chunks, the log files will contain the log
-#' of the old, failed job as well. But \code{showLog} will always jump to the correct part
-#' of the new log file.
+#' of the old, failed job as well. \code{showLog} tries to jump to the correct part
+#' of the new log file with a supported pager.
 #'
 #' @param reg [\code{\link{Registry}}]\cr
 #'   Registry.
 #' @param id [\code{integer(1)}]\cr
 #'   Id of selected job.
 #'   Default is first id in registry.
+#' @param pager [\code{any}]\cr
+#'   Pager to use to display the log. Defaults to \code{getOption("pager")}.
+#'   This option is passed to \code{file.show} and is highly OS dependant and GUI dependant.
+#'   If either R's pager or the environment variable \dQuote{PAGER} is set to \dQuote{less}
+#'   or \dQuote{vim}, the correct part of the log file will be shown.
+#'   Otherwise you find information about the correct part in the beginning of the displayed file.
 #' @return [\code{character(1)}]. Invisibly returns path to log file.
 #' @export
 #' @seealso \code{\link{grepLogs}}, \code{\link{getErrors}}
-showLog = function(reg, id) {
+showLog = function(reg, id, pager=getOption("pager")) {
   checkRegistry(reg)
   syncRegistry(reg)
   if (missing(id)) {
@@ -34,28 +40,28 @@ showLog = function(reg, id) {
   header = c(sprintf("Showing log file for job with id=%i", id),
              sprintf("The string '%s' indicates start of requested job", exact1))
 
-  pager = getOption("pager")
-  sys.pager = Sys.getenv("PAGER")
-  if (grepl("/pager$", pager) && sys.pager != "") {
-    # no user configured pager, we use the system pager
-    pager = sys.pager
-  }
-  bn = basename(strsplit(pager, " ", fixed=TRUE)[[1L]][1L])
+  if (!is.function("pager")) {
+    sys.pager = Sys.getenv("PAGER")
+    if ((grepl("/pager$", pager) || pager == "internal") && sys.pager != "") {
+      # prefer the system pager
+      pager = sys.pager
+    }
+    bn = basename(strsplit(pager, " ", fixed=TRUE)[[1L]][1L])
 
-  # check for less or vim as pagers
-  # if we find the pattern, we jump to the matching line
-  if (bn %in% c("less", "vim")) {
-    pos = grep(exact2, readLines(fn), fixed=TRUE)
-    if (length(pos) == 1L) {
-      pos = pos + length(header) + 1L
-      if(pager == "less")
-        pager = sprintf("%s +%ig", pager, pos)
-      else
-        pager = sprintf("%s +%i", pager, pos)
+    # check for less or vim as pager
+    # if we find the pattern, we jump to the matching line
+    if (bn %in% c("less", "vim")) {
+      pos = grep(exact2, readLines(fn), fixed=TRUE)
+      if (length(pos) == 1L) {
+        pos = pos + length(header) + 1L
+        if(pager == "less")
+          pager = sprintf("%s +%ig", pager, pos)
+        else
+          pager = sprintf("%s +%i", pager, pos)
+      }
     }
   }
 
-  file.show(fn, pager = pager, header=collapse(header, sep="\n"))
-
+  file.show(fn, pager=pager, header=collapse(header, sep="\n"))
   invisible(fn)
 }
