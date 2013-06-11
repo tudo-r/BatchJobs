434611d7abf5dcbe0cacebe2769497ffd10f376c
mllg <michellang@gmail.com>
2013-06-11 11:06:29 +0000

    test for waitForJobs

---
+++ skel/inst/tests/test_waitForJobs.R
@@ -0,0 +1,12 @@
+context("waitForJobs")
+
+test_that("waitForJobs", {
+  f = function(x) { if (x == 5) stop("x == 5") else x }
+  reg = makeTestRegistry()
+  batchMap(reg, f, 1:5)
+  submitJobs(reg)
+  expect_equal(waitForJobs(reg, 1:5, stop.on.error = FALSE), TRUE)
+  expect_equal(waitForJobs(reg, 1:5, stop.on.error = TRUE), FALSE)
+  expect_equal(waitForJobs(reg, 1:4, stop.on.error=FALSE), TRUE)
+  expect_equal(waitForJobs(reg, 1:4, stop.on.error=TRUE), TRUE)
+})
