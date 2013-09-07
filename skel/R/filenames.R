c47188a381af1fdd7f2b3b868e50ff738957b7f4
mllg <michellang@gmail.com>
2013-09-07 21:13:03 +0000

    removed '+' from allowed chars in posix paths

--- skel/R/filenames.R
+++ skel/R/filenames.R
@@ -22,8 +22,9 @@ checkDir = function(path, create=FALSE, check.empty=FALSE, check.posix=FALSE, ms
 
   if (check.posix && getOption("BatchJobs.check.posix", TRUE)) {
     path.abs = makePathAbsolute(path)
-    if(! grepl("^[[:alnum:]:/_.+-]+$", path.abs))
-      stopf("Directory '%s' contains characters that are not fully portable according to POSIX standards. Allowed: a-z A-Z 0-9 : / + . - _", path.abs)
+    # FIXME replace '/' with .Platform$file.sep (-> test on windows if we need any escaping)
+    if(! grepl("^[[:alnum:]:/_.-]+$", path.abs))
+      stopf("Directory '%s' contains characters that are not fully portable according to POSIX standards. Allowed: a-z A-Z 0-9 : / . - _", path.abs)
   }
 }
 
