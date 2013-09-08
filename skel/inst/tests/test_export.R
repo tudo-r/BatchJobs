378b624dee154081601c01e63da59f82a4d8b47d
mllg <michellang@gmail.com>
2013-09-08 17:05:35 +0000

    added comment to failed test

--- skel/inst/tests/test_export.R
+++ skel/inst/tests/test_export.R
@@ -1,11 +1,12 @@
 context("exports")
 
 test_that("exports", {
-  # FIXME more tests
   reg = makeTestRegistry()
   expect_equal(batchExport(reg, x = 1L, y = 2L), c("x", "y"))
   expect_equal(unname(unlist(batchListExported(reg))), c("x", "y"))
   expect_false("x" %in% ls(.GlobalEnv))
+  # this currently fails because of old fail version on cran
+  # update soon or re-write code to not use fail ...
   expect_equal(batchImport(reg), c("x", "y"))
   expect_true(all(c("x", "y") %in% ls(.GlobalEnv)))
   expect_equal(batchUnexport(reg, "x"), "x")
