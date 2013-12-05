5c1a0e384c410db8cde61a1e0e1b23061d82cd70
Bernd Bischl <bernd_bischl@gmx.net>
2013-12-05 14:26:37 +0100

    better test_all.R

--- test_all.R
+++ test_all.R
@@ -1,13 +1,30 @@
 library(methods)
 library(devtools)
 library(testthat)
+library(BBmisc)
+library(fail)
+options(BBmisc.ProgressBar.style = "off")
 
 if (interactive()) {
+  library(RSQLite)
+  library(digest)
   load_all(".")
+  assignConfDefaults()
+  cf = makeClusterFunctionsUnitTests()
+  conf = getBatchJobsConf()
+  conf$interactive = TRUE
 } else {
-  library(BatchJobs)  
+  library("BatchJobs")
+  BatchJobs:::assignConfDefaults()
+  cf = BatchJobs:::makeClusterFunctionsUnitTests()
+  conf = BatchJobs:::getBatchJobsConf()
 }
 
+conf$cluster.functions = cf
+
 source("inst/tests/helpers.R")
+
 test_dir("inst/tests")
 
+# remove bmq stuff
+lapply(list.files("inst/tests/", pattern="bmq_", full.names=TRUE), unlink, recursive=TRUE)
