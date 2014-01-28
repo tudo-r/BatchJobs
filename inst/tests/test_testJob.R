e0213c1e7eeb997722302b77d79b6a6024b1ace4
Michel <michellang@gmail.com>
2014-01-28 18:51:00 +0100

    uncommented some tests

--- inst/tests/test_testJob.R
+++ inst/tests/test_testJob.R
@@ -6,29 +6,27 @@ test_that("testJob", {
   batchMap(reg, identity, 1)
   res = suppressAll(testJob(reg, id, external=FALSE))
   expect_equal(res, 1)
-  # FIXME: shall we reset jobs?
-  # ids = findNotDone(reg)
-  # expect_equal(ids, id)
-  # expect_output({
-  #   st = showStatus(reg)
-  # }, "Status for 1 jobs")
-  # expect_equal(st$submitted, 0)
-  # expect_equal(st$started, 0)
-  # expect_equal(st$done, 0)
-  # expect_equal(st$error, 0)
+  ids = findNotDone(reg)
+  expect_equal(ids, id)
+  expect_output({
+    st = showStatus(reg)
+  }, "Status for 1 jobs")
+  expect_equal(st$submitted, 0)
+  expect_equal(st$started, 0)
+  expect_equal(st$done, 0)
+  expect_equal(st$error, 0)
 
   reg = makeTestRegistry()
   f = function(i) {library(xxxxx);1}
   batchMap(reg, f, 1)
   expect_error(testJob(reg, id, external=FALSE), "xxxxx")
-  # FIXME: shall we reset jobs?
-  # ids = findNotDone(reg)
-  # expect_equal(ids, id)
-  # expect_output({
-  #   df = showStatus(reg)
-  # }, "Status for 1 jobs")
-  # expect_equal(st$submitted, 0)
-  # expect_equal(st$started, 0)
-  # expect_equal(st$done, 0)
-  # expect_equal(st$error, 0)
+  ids = findNotDone(reg)
+  expect_equal(ids, id)
+  expect_output({
+    df = showStatus(reg)
+  }, "Status for 1 jobs")
+  expect_equal(st$submitted, 0)
+  expect_equal(st$started, 0)
+  expect_equal(st$done, 0)
+  expect_equal(st$error, 0)
 })
