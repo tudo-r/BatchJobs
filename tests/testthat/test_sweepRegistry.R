ea02e1f8647ef15b28b6a6ba600bda247c10d6f4
Bernd Bischl <bernd_bischl@gmx.net>
2014-08-15 23:11:55 +0200

    add unit test for sweepRegistry and remove FIXME

---
+++ tests/testthat/test_sweepRegistry.R
@@ -0,0 +1,28 @@
+context("sweepRegistry")
+
+test_that("sweepRegistry", {
+  countFiles = function(reg, type) {
+    fun = switch(type,
+      scripts = getRScriptFilePath,
+      logs = getLogFilePath
+    )
+    fs = sapply(getJobIds(reg), fun, reg = reg)
+    sum(sapply(fs, file.exists))
+  }
+
+  reg = makeTestRegistry()
+  batchMap(reg, function(x) x^2, 1:3)
+  submitJobs(reg)
+  waitForJobs(reg)
+  expect_equal(countFiles(reg, "scripts"), 3L)
+  expect_equal(countFiles(reg, "logs"), 3L)
+  sweepRegistry(reg, "scripts")
+  expect_equal(countFiles(reg, "scripts"), 0L)
+  expect_equal(countFiles(reg, "logs"), 3L)
+  sweepRegistry(reg, "logs")
+  expect_equal(countFiles(reg, "scripts"), 0L)
+  expect_equal(countFiles(reg, "logs"), 0L)
+})
+
+
+
