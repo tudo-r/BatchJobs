9d9c2ce767609b6f323089d02116657098628bbc
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-15 18:11:58 +0200

    add funs to mutate reg.files and dirs

--- tests/testthat/test_sourceRegistryFiles.R
+++ tests/testthat/test_sourceRegistryFiles.R
@@ -33,3 +33,31 @@ test_that("source registry files", {
   res = loadResults(reg, simplify = TRUE, use.names = "none")
   expect_equal(res, 123 + 1:3)
 })
+
+
+test_that("source registry mutators work", {
+  p1 = "unittest-sources"
+  p2 = file.path(getWorkDir(), p1)
+  dir.create(p2, recursive = TRUE)
+  cat("xxx = 123", file = file.path(p2, "test.R"))
+  p3 = file.path(p1, "test.R")
+
+  reg = makeTestRegistry(src.dir = p1)
+  reg = makeTestRegistry()
+  expect_equal(reg$src.files, character(0L))
+  expect_equal(reg$src.dirs, character(0L))
+  reg = addRegistrySourceFiles(reg, p3, src.now = FALSE)
+  expect_equal(reg$src.files, p3)
+  expect_equal(reg$src.dirs, character(0L))
+  reg = addRegistrySourceDirs(reg, p1, src.now = FALSE)
+  expect_equal(reg$src.files, p3)
+  expect_equal(reg$src.dirs, p1)
+  reg = removeRegistrySourceFiles(reg, p3)
+  expect_equal(reg$src.files, character(0L))
+  expect_equal(reg$src.dirs, p1)
+  reg = removeRegistrySourceDirs(reg, p1)
+  expect_equal(reg$src.files, character(0L))
+  expect_equal(reg$src.dirs, character(0L))
+})
+
+
