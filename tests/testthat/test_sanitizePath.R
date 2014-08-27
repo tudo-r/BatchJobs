c56d763833c98a0787653707baf02dedad2af1f7
Michel <michellang@gmail.com>
2014-08-27 14:53:45 +0200

    fix sanitizePath-tests for windows

--- tests/testthat/test_sanitizePath.R
+++ tests/testthat/test_sanitizePath.R
@@ -1,17 +1,27 @@
 context("sanitizePath")
 
 test_that("sanitizePath", {
-  paths = c(tempfile(), "..", "~")
-  expect_identical(sanitizePath(paths), paths)
+  cS = function(x) gsub("\\", "/", x, fixed = TRUE)
 
-  ss = 1:(2L + !isWindows())
-  expect_equal(sanitizePath(paths[ss], normalize = TRUE) == paths, c(TRUE, FALSE, FALSE)[ss])
+  if (!isWindows()) {
+    paths = c(tempfile(), "..", "~")
+    expect_identical(sanitizePath(paths), paths)
+    expect_equal(sanitizePath(paths, normalize = TRUE) == paths, c(TRUE, FALSE, FALSE))
+
+  } else {
+    setwd(tempdir())
+    paths = c(tempfile(), "..")
+    expect_identical(sanitizePath(paths), cS(paths))
+    expect_equal(sanitizePath(paths, normalize = TRUE) == cS(paths), c(TRUE, FALSE))
+  }
 
   path = "c:\\temp"
-  expect_false(sanitizePath(path, normalize = TRUE) == path)
   expect_false(sanitizePath(path, normalize = FALSE) == path)
+  expect_identical(sanitizePath(path, normalize = FALSE), cS(path))
+  expect_identical(sanitizePath(path, normalize = TRUE), cS(path))
 
   path = file.path(tempdir(), "..", fsep = "/")
-  expect_false(sanitizePath(path, normalize = TRUE) == path)
-  expect_true(sanitizePath(path, normalize = FALSE) == path)
+  expect_false(sanitizePath(path, normalize = TRUE) == cS(path))
+  expect_identical(sanitizePath(path, normalize = FALSE), cS(path))
+  expect_identical(sanitizePath(path, normalize = TRUE), cS(dirname(dirname(path))))
 })
