5d52d88a1c413beadafea07474e70cb1ef3fed0f
Michel <michellang@gmail.com>
2014-09-10 15:06:19 +0200

    fix reg id check (must start with alphabetic char for sqlite)

--- R/checkIdValid.R
+++ R/checkIdValid.R
@@ -1,5 +1,5 @@
 # check for a valid registry id:
-# must start with an alpha-numeric
+# must start with an alphabetic
 # then alpha-numerics and underscores are allowed
 # pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z_-]*$"
 # we must be quite restrictive here because we prefix
@@ -7,9 +7,9 @@
 checkIdValid = function(id, allow.minus = TRUE) {
   assertString(id)
   if (allow.minus)
-    pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z_-]*$"
+    pattern = "^[a-zA-Z]+[0-9a-zA-Z_-]*$"
   else
-    pattern = "^[0-9a-zA-Z]+[0-9a-zA-Z_]*$"
+    pattern = "^[a-zA-Z]+[0-9a-zA-Z_]*$"
   if (!grepl(pattern, id))
     stopf("Id does not comply with pattern %s: %s", pattern, id)
 }
