7831b8d39d2e34ffeb7a66c938feb3da8323bec0
Michel <michellang@gmail.com>
2014-09-10 17:30:30 +0200

    fix test for darwin

--- tests/testthat/test_sanitizePath.R
+++ tests/testthat/test_sanitizePath.R
@@ -3,15 +3,21 @@ context("sanitizePath")
 test_that("sanitizePath", {
   cS = function(x) gsub("\\", "/", x, fixed = TRUE)
 
-  if (!isWindows()) {
+  if (isLinux()) {
     setwd(tempdir())
     paths = c(tempfile(), "..", "~")
     expect_identical(sanitizePath(paths, make.absolute = TRUE), c(paths[1], dirname(tempdir()), path.expand("~")))
     expect_equal(sanitizePath(paths, make.absolute = TRUE) == paths, c(TRUE, FALSE, FALSE))
-  } else {
+  } else if (isWindows()) {
     setwd(tempdir())
     paths = c(tempfile(), "..")
     expect_identical(sanitizePath(paths), cS(c(paths[1], dirname(tempdir()))))
+    expect_equal(sanitizePath(paths, normalize = TRUE) == cS(paths), c(TRUE, FALSE))
+  } else if (isDarwin()) {
+    # mac os often has a symlinked tempdir
+    setwd(tempdir())
+    paths = c(tempfile(), "~")
+    expect_identical(sanitizePath(paths, make.absolute = TRUE), cS(c(paths[1], path.expand("~"))))
     expect_equal(sanitizePath(paths, normalize = TRUE) == cS(paths), c(TRUE, FALSE))
   }
 
@@ -23,5 +29,6 @@ test_that("sanitizePath", {
   path = file.path(tempdir(), "..", fsep = "/")
   expect_false(sanitizePath(path, make.absolute = TRUE, normalize.absolute = TRUE) == cS(path))
   expect_identical(sanitizePath(path, make.absolute = FALSE), cS(path))
-  expect_identical(sanitizePath(path, make.absolute = TRUE, normalize.absolute = TRUE), cS(dirname(dirname(path))))
+  if (!isDarwin())
+    expect_identical(sanitizePath(path, make.absolute = TRUE, normalize.absolute = TRUE), cS(dirname(dirname(path))))
 })
