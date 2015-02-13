47bd9925476eddd83932b87bb2ca171ab5b5e52c
hb <hb@aroma-project.org>
2015-02-12 17:19:03 -0800

    Added batchEvalQ()

---
+++ R/batchEvalQ.R
@@ -0,0 +1,22 @@
+#' Evaluates expressions as jobs on a registry
+#'
+#' @param reg [\code{\link{Registry}}]\cr
+#'   Empty Registry that will store jobs for the mapping.
+#' @param exprs [\code{\link{expression}}]\cr
+#'   R expressions to evaluate.
+#' @param ... [any]\cr
+#'   Additional arguments passed to \code{\link{batchMap}}.
+#'
+#' @return Vector of type \code{integer} with job ids.
+#'
+#' @seealso This function is analogous to
+#' \code{parallel::\link[parallel]{clusterEvalQ}}.
+#'
+#' @export
+batchEvalQ <- function(reg, exprs, ...) {
+  fun <- function(expr, ..., envir=parent.frame()) {
+    eval(expr, envir=envir)
+  }
+  batchMap(reg, fun=fun, exprs, ...)
+}
+
