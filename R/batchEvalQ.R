ee6dbdea04c3a051213636b211d999cd83a28122
hb <hb@aroma-project.org>
2015-02-13 21:31:27 -0800

    batchEvalQ() gained argument 'local'

--- R/batchEvalQ.R
+++ R/batchEvalQ.R
@@ -4,6 +4,10 @@
 #'   Empty Registry that will store jobs for the mapping.
 #' @param exprs [\code{\link{expression}}]\cr
 #'   R expressions to evaluate.
+#' @param local [\code{\link{logical}}[1]]\cr
+#'   If TRUE, the expression is evaluated wrapped in a
+#'   \code{\link[base]{local}} call, otherwise directly
+#'   in the global environment (of the job).
 #' @param ... [any]\cr
 #'   Additional arguments passed to \code{\link{batchMap}}.
 #'
@@ -13,10 +17,15 @@
 #' \code{parallel::\link[parallel]{clusterEvalQ}}.
 #'
 #' @export
-batchEvalQ <- function(reg, exprs, ...) {
-  fun <- function(expr, ..., envir=globalenv()) {
-    eval(substitute(expr), envir=envir)
+batchEvalQ <- function(reg, exprs, local=FALSE, ...) {
+  if (local) {
+    fun <- function(expr, ..., envir=globalenv()) {
+      eval(substitute(local(expr)), envir=envir)
+    }
+  } else {
+    fun <- function(expr, ..., envir=globalenv()) {
+      eval(substitute(expr), envir=envir)
+    }
   }
   batchMap(reg, fun=fun, exprs, ...)
 }
-
