238e5a67f56ace9402fc72c0b0952ddd11ab869c
openlava <openlava@node3.(none)>
2015-03-24 13:50:35 +0000

    fix mpi exit in example to keep consistency between R job and OL job

--- external_tests/openlava-rmpi.R
+++ external_tests/openlava-rmpi.R
@@ -17,6 +17,7 @@ f = function(x) {
   mpi.spawn.Rslaves(nslaves=slaveno)
   mpi.remote.exec(paste("I am",mpi.comm.rank(),"of",mpi.comm.size()))
   mpi.close.Rslaves()
+  mpi.exit()
   
 }
 batchMap(reg, f, 1)
