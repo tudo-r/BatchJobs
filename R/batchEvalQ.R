1f966d31206a2b77f390866956fbbf36789cd54d
hb <hb@aroma-project.org>
2015-05-09 20:12:54 -0700

    Add example() and improved help() for batchEvalQ() [#72]

--- R/batchEvalQ.R
+++ R/batchEvalQ.R
@@ -2,8 +2,8 @@
 #'
 #' @param reg [\code{\link{Registry}}]\cr
 #'   Empty Registry that will store jobs for the mapping.
-#' @param exprs [\code{\link{expression}}]\cr
-#'   R expressions to evaluate.
+#' @param exprs [\code{\link{list}}]\cr
+#'   A list of \code{\link{expression}}:s to evaluate.
 #' @param local [\code{\link{logical}}[1]]\cr
 #'   If TRUE, the expression is evaluated wrapped in a
 #'   \code{\link[base]{local}} call, otherwise directly
@@ -13,11 +13,27 @@
 #'
 #' @return Vector of type \code{integer} with job ids.
 #'
-#' @seealso This function is analogous to
-#' \code{parallel::\link[parallel]{clusterEvalQ}}.
+#' @seealso This function work analogously to
+#' \code{parallel::\link[parallel]{clusterEvalQ}} but
+#' accepts multiple expressions instead of just one.
+#'
+#' @examples
+#' reg = makeRegistry(id = "BatchJobsExample", file.dir = tempfile(), seed = 123)
+#' exprs <- list(
+#'   A = substitute({
+#'     rnorm(10)
+#'   }),
+#'   B = substitute({
+#'     rnorm(20)
+#'   })
+#' )
+#' batchEvalQ(reg, exprs)
+#' print(reg)
 #'
 #' @export
 batchEvalQ <- function(reg, exprs, local=FALSE, ...) {
+  assertList(exprs)
+
   if (local) {
     fun <- function(expr, ..., envir=globalenv()) {
       eval(substitute(local(expr)), envir=envir)
@@ -27,5 +43,6 @@ batchEvalQ <- function(reg, exprs, local=FALSE, ...) {
       eval(substitute(expr), envir=envir)
     }
   }
+
   batchMap(reg, fun=fun, exprs, ...)
 }
--- tests/testthat/test_batchEvalQ.R
+++ tests/testthat/test_batchEvalQ.R
@@ -4,7 +4,7 @@ test_that("batchEvalQ", {
   ## Global string, which should be found instead of base::url().
   url <- "http://www.r-project.org"
 
-  ## Expression to be evaluated
+  ## A single expression to be evaluated
   expr <- substitute({
     cat("URL:\n")
     print(url)
@@ -16,10 +16,32 @@ test_that("batchEvalQ", {
   reg = makeTestRegistry()
   res = batchExport(reg, li=list(url=url))
   ids = batchEvalQ(reg, exprs=list(expr))
+  expect_identical(length(ids), 1L)
 
   submitJobs(reg)
   waitForJobs(reg)
 
   res = sapply(getJobIds(reg), function(id) loadResult(reg, id))
+  expect_identical(length(res), length(ids))
   expect_identical(res, url)
+
+
+  ## Multiple expressions to be evaluated
+  exprs <- list(
+    A = substitute({ x <- 2; x^2 }),
+    B = substitute({ x <- 3; x^2 }),
+    C = substitute({ x <- 4; x^2 })
+  )
+  print(exprs)
+
+  reg = makeTestRegistry()
+  ids = batchEvalQ(reg, exprs=exprs)
+  expect_identical(length(ids), 3L)
+
+  submitJobs(reg)
+  waitForJobs(reg)
+
+  res = sapply(getJobIds(reg), function(id) loadResult(reg, id))
+  expect_identical(length(res), length(ids))
+  expect_identical(res, c(4, 9, 16))
 })
