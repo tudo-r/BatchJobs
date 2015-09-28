c0d8f748c386faaeae5a8584e69665fec97cf51d
Michel <michellang@gmail.com>
2015-09-28 11:55:11 +0200

    fix broken buffer

--- R/buffer.R
+++ R/buffer.R
@@ -13,23 +13,25 @@ buffer = function(type = "list", capacity = 0L, value = TRUE, init = NULL, ...) 
   force(value)
   ddd = list(...)
 
+  clear = function() {
+    if (is.function(value))
+      ret = do.call(value, c(list(head(st, n)), ddd))
+    else
+      ret = value
+    n <<- 0L
+    ret
+  }
+
   list(
     get = function() {
       head(st, n)
     },
+    clear = clear,
     push = function(x) {
       if (n == capacity)
         clear()
       n <<- n + 1L
       st[[n]] <<- x
-    },
-    clear = function() {
-      if (is.function(value))
-        ret = do.call(value, c(list(head(st, n)), ddd))
-      else
-        ret = value
-      n <<- 0L
-      ret
     },
     pos = function() {
       n
