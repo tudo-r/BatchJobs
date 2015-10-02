323e094520b62eeb66b2d4f94d8432a6298a3d1f
Michel <michellang@gmail.com>
2015-10-02 10:17:15 +0200

    new export: getJobLocation (export of getJobDirs)

---
+++ R/getJobLocation.R
@@ -0,0 +1,8 @@
+#' Get the physical location of job files on the hard disk.
+#' @template arg_reg
+#' @template arg_ids
+#' @return [\code{character}] Vector of directories.
+getJobLocation = function(reg, ids) {
+  checkRegistry(reg)
+  getJobDirs(reg, ids)
+}
